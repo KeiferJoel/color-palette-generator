3cfdd452a88cdc738420062f9199ddfb79537f9f
Joel Pichardo <joelalbertopichardoalayon@gmail.com>
2026-07-03 16:31:49 -0400

    feat: release v2.0 with redesign UI and new features

--- script.js
+++ script.js
@@ -1,34 +1,102 @@
 const palette = document.getElementById("palette");
 const generateBtn = document.getElementById("generateBtn");
+const toast = document.getElementById("toast");
 
-function randomColor() {
+function randomColor(){
+
     const letters = "0123456789ABCDEF";
 
     let color = "#";
 
-    for (let i = 0; i < 6; i++) {
+    for(let i = 0; i < 6; i++){
+
         color += letters[Math.floor(Math.random() * 16)];
+
     }
 
     return color;
 }
 
-function generatePalette() {
+function showToast(message){
 
-    const boxes = document.querySelectorAll(".color-box");
+    toast.textContent = message;
 
-    boxes.forEach(box => {
+    toast.classList.add("show");
 
-        const color = randomColor();
+    setTimeout(()=>{
 
-        box.style.backgroundColor = color;
+        toast.classList.remove("show");
 
-        box.querySelector("span").textContent = color;
-
-    });
+    },1500);
 
 }
 
-generateBtn.addEventListener("click", generatePalette);
+function generatePalette(){
+
+    palette.innerHTML = "";
+
+    for(let i = 0; i < 5; i++){
+
+        const color = randomColor();
+
+        const card = document.createElement("div");
+
+        card.className = "color-card";
+
+        card.innerHTML = `
+
+            <div
+                class="color"
+                style="background:${color}"
+            ></div>
+
+            <div class="info">
+
+                <span>${color}</span>
+
+                <i class="fa-regular fa-copy"></i>
+
+            </div>
+
+        `;
+
+        card.addEventListener("click",()=>{
+
+            navigator.clipboard.writeText(color);
+
+            showToast(`${color} copied!`);
+
+        });
+
+        palette.appendChild(card);
+
+    }
+
+}
+
+generateBtn.addEventListener(
+
+    "click",
+    generatePalette
+
+);
+
+document.addEventListener(
+
+    "keydown",
+
+    e=>{
+
+        if(e.code === "Space"){
+
+            e.preventDefault();
+
+            generatePalette();
+
+        }
+
+    }
+
+);
 
 generatePalette();
